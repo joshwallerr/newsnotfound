c79b14e63967f9bd6f6ecf2bbc1c2637c9319c53
joshwallerr <joshwaller101@hotmail.com>
2023-06-03 15:03:50 +0100

    Added support for excluded words for each category when choosing headlines. This aims to solve the problem of the World category being flooded with news about Ukraine, and allows us to separate Ukraine news into its own category

--- choose_headlines.py
+++ choose_headlines.py
@@ -3,6 +3,10 @@ import ast
 import random
 import os
 
+excluded_words = {
+    'world': ['ukraine'],
+}
+
 def find_most_suitable_headlines(category):
     """
     This function reads and filters headlines from CSV files based on a given category, and returns the
@@ -21,9 +25,16 @@ def find_most_suitable_headlines(category):
         next(reader)  # skip header
         covered_headlines = [row[0] for row in reader]
 
+    def exclude_words(headline):
+        # Check if the headline contains any of the excluded words for the given category
+        if category in excluded_words:
+            return any(word in headline.lower() for word in excluded_words[category])
+        else:
+            return False
+
     def choose_headlines(headlines):
         headlines = sorted(headlines, key=lambda x: x[1], reverse=True)
-        headlines = [x[0] for x in headlines if x[0] not in covered_headlines]
+        headlines = [x[0] for x in headlines if x[0] not in covered_headlines and not exclude_words(x[0])]
         return headlines[:3]
 
     def should_skip_list(headlines):
@@ -31,6 +42,8 @@ def find_most_suitable_headlines(category):
         if len(headlines) > 2 and covered_count >= 2:
             return True
         if len(headlines) <= 2 and covered_count >= 1:
+            return True
+        if any(exclude_words(x[0]) for x in headlines):  # Check if all headlines in the list contain excluded words
             return True
         return False
 
@@ -52,7 +65,7 @@ def find_most_suitable_headlines(category):
             all_headlines = [row[category_index] for row in reader if row[category_index]]
 
         # Choose a random headline that hasn't been covered
-        random_headline = random.choice([x for x in all_headlines if x not in covered_headlines])
+        random_headline = random.choice([x for x in all_headlines if x not in covered_headlines and not exclude_words(x)])
         chosen_headlines = [random_headline]
 
     for headline in chosen_headlines:
